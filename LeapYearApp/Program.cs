30865194b7782e934fb38ea5cad1f8609c9be446
Dean Justine Suficiencia <djsuficiencia@csmbataan.ph.education>
2024-09-26 17:39:26 +0800

    Activity 4

---
+++ LeapYearApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeapYearApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (IsLeapYear(year))
+            {
+                Console.WriteLine($"{year} is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not a leap year.");
+            }
+        }
+
+        static bool IsLeapYear(int year)
+        {
+            // Check if a year is a leap year
+            return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+        }
+    }
+}
